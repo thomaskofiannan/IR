1ed19e882d457ca1a70f0cb1c9e73ecf31fefb54
thomaskofiannan <thomaskofiannan@gmail.com>
2016-02-16 13:23:55 -0500

    Navigation drawer for the home page

--- app/src/main/java/cs/nsu/edu/ir/activities/SplashActivity.java
+++ app/src/main/java/cs/nsu/edu/ir/activities/SplashActivity.java
@@ -1,4 +1,4 @@
-package cs.nsu.edu.ir;
+package cs.nsu.edu.ir.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import cs.nsu.edu.ir.R;
 
 public class SplashActivity extends AppCompatActivity {
 
